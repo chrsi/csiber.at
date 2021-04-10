273e711c6d21f130727c8ae58cbba828ee87fa99
Christian Siber <siber.christian@gmail.com>
2021-04-10 14:45:39 +0200

    🎸 feat(blog): Setup CORS for the API

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,22 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const blogController = require('./controller/blogController')
 
 const app = express()
 const port = process.env.PORT;
+
+var whitelist = ['http://localhost:3000', 'https://csiber.at']
+app.use(cors({
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}))
 
 app.use('/api/blog', blogController);
 
